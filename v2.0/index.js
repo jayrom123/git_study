fc88bd7b58f896b140dc3c773bebbe8859dfc883
LuoWenjie <wenjie.luo@56qq.com>
2018-12-10 18:50:01 +0800

    feat(): 添加ignore

--- v2.0/index.js
+++ v2.0/index.js
@@ -1,3 +1,24 @@
 export const name = 'roger';
 
 export const a = 'v2.0';
+
+
+function nest(fn, currentArgs = [], allArgs = []) {
+    let newArgs = [...allArgs, ...currentArgs];
+    if (newArgs.length === fn.length) {
+        return fn(...newArgs);
+    } else {
+        return (...args) => {
+            nest(args, newArgs, fn)
+        }
+    }
+}
+
+function curry(fn) {
+    if (fn.length === 0) {
+        return fn;
+    }
+    retrun(
+        (...arg) => nest(arg)
+    )
+}
